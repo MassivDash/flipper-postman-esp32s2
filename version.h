35a2769412777646ebef8c168ba0449abb7bdfc4
SpaceGhost <lukasz.celitan@gmail.com>
2024-10-28 22:02:16 +0100

    docs: bump version

--- version.h
+++ version.h
@@ -5,6 +5,6 @@
 /**
  * @brief Version number imported by the help and console splash functions.
  */
-static const char *version = "0.1.10";
+static const char *version = "0.1.11";
 
 #endif // VERSION_H
